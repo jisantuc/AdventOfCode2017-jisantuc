2bff3bf0c5f80e118b7db809ba8705e3e7810c90
jisantuc <james.santucci@gmail.com>
2017-12-01 17:20:15 -0500

    Use zipWith and do part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,19 @@
 module Day1 where
 
 import Data.Maybe
+import Data.Semigroup
 
 -- Part 1
 -- sums of all numbers in a string of numbers that are the same as the next value
 -- in the string
+
+newtype CombinyInt = CombinyInt Integer deriving (Eq, Show)
+
+instance Semigroup CombinyInt where
+  (<>) x1 x2 = if x1 == x2 then x1 else CombinyInt 0
+
+fromCombinyInt :: CombinyInt -> Integer
+fromCombinyInt (CombinyInt x) = x
 
 allNums :: String
 allNums = "0123456789"
@@ -15,21 +24,20 @@ readInteger x
   | elem (head x) allNums = Just (read x)
   | otherwise = Nothing
 
-numsToNumList :: [Integer] -> [Char] -> [Integer]
+numsToNumList :: [CombinyInt] -> [Char] -> [CombinyInt]
 numsToNumList accum [] = accum
 numsToNumList accum (x : xs)
-  | isJust (readInteger [x]) = numsToNumList (accum ++ [read [x]]) xs
+  | isJust (readInteger [x]) = numsToNumList (accum ++ [CombinyInt $ read [x]]) xs
   | otherwise = numsToNumList accum xs
 
-wrappedValueFromNumString :: String -> Integer
-wrappedValueFromNumString [] = 0
-wrappedValueFromNumString (x : xs)
-  | isJust (readInteger [x]) = read [x] :: Integer
-  | otherwise = 0
+sumRuns :: [CombinyInt] -> Integer
+sumRuns nums = sum . map fromCombinyInt
+  $ zipWith (<>) nums (tail nums ++ take 1 nums)
 
-sumRuns :: Integer -> Integer -> [Integer] -> Integer
-sumRuns accum _ [] = accum
-sumRuns accum initial (x: []) = if (x == initial) then accum + x else accum
-sumRuns accum initial (x1 : x2 : xs) = if (x1 == x2)
-  then (sumRuns (accum + x1) initial (x2:xs))
-  else (sumRuns accum initial (x2:xs))
+-- Part 2
+-- the same, but for numbers halfway around the list
+sumSymmetricalNums :: [CombinyInt] -> Integer
+sumSymmetricalNums nums = sum . map fromCombinyInt
+  $ zipWith (<>) nums (drop half nums ++ take half nums)
+  where
+    half = div (length nums) 2
